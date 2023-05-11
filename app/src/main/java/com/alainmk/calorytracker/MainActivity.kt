b8ddb2b9461263d56e9627c5fc9d62392160406e
Alain MK <42724442+AlainMk@users.noreply.github.com>
2023-05-11 18:40:04 +0100

    adding age feature

--- app/src/main/java/com/alainmk/calorytracker/MainActivity.kt
+++ app/src/main/java/com/alainmk/calorytracker/MainActivity.kt
@@ -3,7 +3,12 @@ package com.alainmk.calorytracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -11,6 +16,7 @@ import androidx.navigation.compose.rememberNavController
 import com.alainmk.calorytracker.navigation.navigate
 import com.alainmk.calorytracker.ui.theme.CaloryTrackerTheme
 import com.alainmk.core.navigation.Route
+import com.alainmk.onboarding_presentation.age.AgeScreen
 import com.alainmk.onboarding_presentation.gender.GenderScreen
 import com.alainmk.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,40 +28,49 @@ class MainActivity : ComponentActivity() {
         setContent {
             CaloryTrackerTheme {
                 val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = Route.WELCOME
-                ) {
-                    composable(Route.WELCOME) {
-                        WelcomeScreen(onNavigate = navController::navigate)
-                    }
-                    composable(Route.AGE) {
+                val scaffoldState = rememberScaffoldState()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
+                ) { p ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME
+                    ) {
+                        composable(Route.WELCOME) {
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.AGE) {
+                            AgeScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.GENDER) {
+                            GenderScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.HEIGHT) {
 
-                    }
-                    composable(Route.GENDER) {
-                        GenderScreen(onNavigate = navController::navigate)
-                    }
-                    composable(Route.HEIGHT) {
+                        }
+                        composable(Route.WEIGHT) {
 
-                    }
-                    composable(Route.WEIGHT) {
+                        }
+                        composable(Route.NUTRIENT_GOAL) {
 
-                    }
-                    composable(Route.NUTRIENT_GOAL) {
+                        }
+                        composable(Route.ACTIVITY) {
 
-                    }
-                    composable(Route.ACTIVITY) {
+                        }
+                        composable(Route.GOAL) {
 
-                    }
-                    composable(Route.GOAL) {
+                        }
 
-                    }
+                        composable(Route.TRACKER_OVERVIEW) {
 
-                    composable(Route.TRACKER_OVERVIEW) {
+                        }
+                        composable(Route.SEARCH) {
 
-                    }
-                    composable(Route.SEARCH) {
-
+                        }
                     }
                 }
             }
--- app/src/main/java/com/alainmk/calorytracker/di/AppModule.kt
+++ app/src/main/java/com/alainmk/calorytracker/di/AppModule.kt
@@ -5,6 +5,7 @@ import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import com.alainmk.core.data.preferences.DefaultPreferences
 import com.alainmk.core.domain.preferences.Preferences
+import com.alainmk.core.use_case.FilterOutDigits
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -26,5 +27,11 @@ object AppModule {
     @Singleton
     fun providePreferences(sharedPreferences: SharedPreferences): Preferences {
         return DefaultPreferences(sharedPreferences)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFilterOutDigitsUseCase(): FilterOutDigits {
+        return FilterOutDigits()
     }
 }
