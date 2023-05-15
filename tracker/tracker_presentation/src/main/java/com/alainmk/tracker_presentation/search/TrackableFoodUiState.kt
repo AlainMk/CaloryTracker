15d13139767ac8e98782f98d9dcd4d05d4a54aeb
Alain MK <42724442+AlainMk@users.noreply.github.com>
2023-05-15 16:47:50 +0100

    implementing search ui logic

---
+++ tracker/tracker_presentation/src/main/java/com/alainmk/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.alainmk.tracker_presentation.search
+
+import com.alainmk.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
